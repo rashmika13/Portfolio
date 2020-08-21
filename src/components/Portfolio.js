505cf52c4dd9883a2bb99fa39c6ebcf8d0db4487
R <50966008+rashmika13@users.noreply.github.com>
2020-08-20 21:17:16 -0400

    portfolio  page started

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,54 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { Box, Grid, Card, CardActionArea } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+// import project1 from "../images/";
+import {
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
 function Portfolio() {
   return (
-    <>
+    <Box>
       <Navbar />
-      <h1>My portfolio</h1>
-    </>
+      <Grid container justify="center" alignItems="center">
+        <Grid item xs={12} sm={8} md={6}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Project1"
+                height="140"
+                // image={project1}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  Project 1
+                </Typography>
+                <Typography variant="body" color="textSecondary" component="p">
+                  Lorem Ipsum
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Live DEmo
+                </Button>
+              </CardActions>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
